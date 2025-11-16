ed6685c931ed2d41816b6ef5e0240381b557a6bc
satriaberliano <sanozisb@gmail.com>
2025-11-17 02:57:53 +0700

    add: back to top button

--- script.js
+++ script.js
@@ -7,5 +7,27 @@ function onButtonClickAlert() {
 
 buttonContact.addEventListener("click", onButtonClickAlert);
 
+// Back to Top Button
+const backToTopButton = document.getElementById("back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+}
+
+function backToTopFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+backToTopButton.addEventListener("click", backToTopFunction);
+
 // Footer
 document.getElementById("year-footer").innerHTML = new Date().getFullYear();
